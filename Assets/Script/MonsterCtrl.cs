6c2f316f221ea940224f4cea0d86972f23932b47
Ominium <gustjr790@gmail.com>
2019-04-25 17:27:49 +0900

    Startcommit

---
+++ Assets/Script/MonsterCtrl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCtrl : MonoBehaviour
+{
+   
+    public GameObject M_map_ob;
+    public MonsterDatabase M_mondata_data;
+    Transform M_gen_tr;
+    public GameObject[] M_monster_ob = new GameObject[5]; // 몬스터 종류
+    public int M_count = 0;
+    public void StartRound() // 유닛 생성
+    {
+        GameSystem.Instatce.G_playing = false;
+        for (int k=0; k < M_map_ob.transform.childCount; k++) // 생성 위치 찾기
+        {
+          if(M_map_ob.transform.GetChild(0).transform.GetChild(k).GetComponent<Material>()
+                == Info.Instatnce.I_node_mat[4])
+            {
+                M_gen_tr = M_map_ob.transform.GetChild(0).transform.GetChild(k).transform;
+            }
+        }
+       Instantiate(M_monster_ob[GameSystem.Instatce.G_wave], M_gen_tr);
+        if (M_count < GameSystem.Instatce.G_roundunit[GameSystem.Instatce.G_wave] - 1)
+        {
+            
+            M_count++;
+            Invoke("StartRound", GameSystem.Instatce.G_roundgen);
+        }
+        else M_count = 0;
+    }
+    void Awake() 
+    {
+        for(int i=0; i < M_monster_ob.Length; i++) {
+            M_monster_ob[i] = M_mondata_data.MonstersList[i].M_prefab_ob;
+        }
+      
+    }
+    void Start()
+    {
+        
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+}
---
+++ Assets/Script/MonsterMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMove : MonoBehaviour
+{
+    public TilePass M_tilePass;
+    int M_count = 0;
+    void Awake()
+    {
+        M_tilePass = GameObject.Find("TilePass").GetComponent<TilePass>();
+    }
+ 
+    void Update()
+    {
+        Move();        
+    }
+    void Move() // 길찾기 함수
+    {
+        transform.position = Vector3.MoveTowards(transform.position,
+         M_tilePass.Get_T_tile_tr(M_count).position, Time.deltaTime * 3);
+        if (Vector3.Distance(transform.position, M_tilePass.Get_T_tile_tr(M_count).position) < 0.2f)
+        {
+           M_count++;
+            if (M_count == M_tilePass.T_tile_tr.Length)
+            {
+                Player.P_hp -= 3;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
